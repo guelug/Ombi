5538e6dafafd425ce1deb54f4032a73bf67ed9df
Pedro <guelug@gmail.com>
2024-11-27 10:59:31 +0100

    Update button.component.ts
    add a functionality for "I have this movie"

--- src/Ombi/ClientApp/src/app/components/button/button.component.ts
+++ src/Ombi/ClientApp/src/app/components/button/button.component.ts
@@ -1,6 +1,8 @@
 import { OmbiCommonModules } from "../modules";
-import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { ChangeDetectionStrategy, Component, Input, Output, EventEmitter } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
+
+export type ButtonType = 'primary' | 'havemovie'; // Add other existing types if any
 
 @Component({
     standalone: true,
@@ -8,17 +10,32 @@ import { MatButtonModule } from "@angular/material/button";
     imports: [...OmbiCommonModules, MatButtonModule],
     changeDetection: ChangeDetectionStrategy.OnPush,
     template: `
-    <button [id]="id" [type]="type" [class]="class" [data-toggle]="dataToggle" mat-raised-button [data-target]="dataTarget">{{text}}</button>
+    <button 
+        [id]="id" 
+        [type]="type" 
+        [class]="class" 
+        [data-toggle]="dataToggle" 
+        [data-target]="dataTarget"
+        mat-raised-button
+        [color]="type === 'havemovie' ? 'accent' : 'primary'"
+        (click)="handleClick()">
+        {{text}}
+    </button>
     `
-  })
-  export class ButtonComponent {
-
+})
+export class ButtonComponent {
     @Input() public text: string;
-
     @Input() public id: string;
-    @Input() public type: string = "primary";
+    @Input() public type: ButtonType = "primary";
     @Input() public class: string;
     @Input('data-toggle') public dataToggle: string;
     @Input('data-target') public dataTarget: string;
 
-  }
+    @Output() public haveMovieClick = new EventEmitter<void>();
+
+    handleClick() {
+        if (this.type === 'havemovie') {
+            this.haveMovieClick.emit();
+        }
+    }
+}
