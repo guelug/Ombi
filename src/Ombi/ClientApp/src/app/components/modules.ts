323594bb2c712aa2b29abcf480543d48b9cbf2cd
Pedro <guelug@gmail.com>
2024-11-29 11:44:29 +0100

    Update modules.ts
    updated angular components!

--- src/Ombi/ClientApp/src/app/components/modules.ts
+++ src/Ombi/ClientApp/src/app/components/modules.ts
@@ -1,3 +1,12 @@
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
 
+export const OmbiCommonModules = [
+  CommonModule,
+  MatButtonModule,
+  MatTableModule,
+  MatIconModule,
+];
 export const OmbiCommonModules = [CommonModule];
